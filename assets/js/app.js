9040b3fe0455fa26f212cbc69fb617d0fb5097fc
sinozukamiiina <126172829+sinozukamiiina@users.noreply.github.com>
2024-03-06 15:39:05 +0000

    重みづけ

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,6 +18,17 @@ document.addEventListener('DOMContentLoaded', function() {
                 borderWidth: 1
             }]
         },
+        // type: 'bar', // グラフのタイプ
+        // data: {
+        //     labels: Array.from({length: 20}, (_, i) => i + 1), // X軸のラベル（1から20まで）
+        //     datasets: [{
+        //         label: '1の目が初めて出るまでの試行回数の確率 (%)',
+        //         data: [16.67, 13.89, 11.57, 9.65, 8.04, 6.70, 5.58, 4.65, 3.88, 3.23, 2.69, 2.24, 1.87, 1.56, 1.30, 1.08, 0.90, 0.75, 0.63, 0.52], // Y軸のデータ
+        //         backgroundColor: 'rgba(255, 99, 132, 0.2)', // バーカラー
+        //         borderColor: 'rgba(255, 99, 132, 1)', // バーボーダーカラー
+        //         borderWidth: 1
+        //     }]
+        // },
         options: {
             scales: {
                 y: {
@@ -30,11 +41,19 @@ document.addEventListener('DOMContentLoaded', function() {
     // 入力フィールドを追加する関数
     function addInputField() {
         var container = document.getElementById('inputContainer');
+        
         var newInput = document.createElement('input');
         newInput.type = 'number';
         newInput.className = 'inputValue';
-        newInput.placeholder = '例: 2.5';
+        newInput.placeholder = '確率: 1.5';
         container.appendChild(newInput);
+
+        var newWeight = document.createElement('input');
+        newWeight.type = 'number';
+        newWeight.className = 'inputWeight';
+        newWeight.placeholder = '確保数: 2';
+        newWeight.value = 1; // デフォルトの重みは1
+        container.appendChild(newWeight);
     }
 
     // 最初の入力フィールドを追加
@@ -47,14 +66,15 @@ document.addEventListener('DOMContentLoaded', function() {
 
     document.getElementById('calculate').addEventListener('click', function() {
         var inputs = document.querySelectorAll('.inputValue');
+        var weight = document.querySelectorAll('.inputWeight');
         var total = 0;
         var count = 0;
 
         inputs.forEach(function(input) {
             var value = parseFloat(input.value);
             if (!isNaN(value)) {
-                total += value;
-                count++;
+                total += value * weight;
+                count += weight;
             }
         });
 
