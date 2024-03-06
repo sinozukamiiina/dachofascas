847e55f9baabe3097acc67dc43fd5fd37b052b99
sinozukamiiina <126172829+sinozukamiiina@users.noreply.github.com>
2024-03-06 12:23:56 +0000

    グラフとスクリプト参照変更

---
+++ assets/js/chart-setup.js
@@ -0,0 +1,30 @@
+// chart-setup.js
+document.addEventListener('DOMContentLoaded', function() {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['入力値', '出力値'], // X軸のラベル
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19], // ここに入力値と出力値を入れます
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+});
